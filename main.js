5a13885da5d848614cbeaf8ce707f1b9df3cd31e
Erica Wright <ericaewright@gmail.com>
2016-11-25 14:58:52 -0500

    style stuff

--- main.js
+++ main.js
@@ -91,31 +91,57 @@ function firstInstallTour(win) {
     let document = win.document;
     let button = document.getElementById('side-tabs-button');
     let panel = document.createElement('panel');
-    let outerbox = document.createElement('vbox', 'onboard-panel-box');
+    let outerbox = document.createElement('vbox');
     let instructions = document.createElement('description');
-    let progressButton = document.createElement('button', 'step-one-button');
+    let progressButton = document.createElement('button');
+    let tourTitle = document.createElement('h2');
+    let tourVideo = document.createElement('img');
+    let dismissLabel = document.createElement('label');
     document.getElementById('mainPopupSet').appendChild(panel); //attach to DOM anywhere
     panel.setAttribute('id', 'tour-panel');
     panel.setAttribute('type', 'arrow');
     panel.setAttribute('flip', 'slide');
     panel.style.width = '200px';
     panel.style.height = '400px';
-    instructions.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+
+    tourTitle.textContent = 'Tame Your Tabs!';
+    tourTitle.style.fontSize = '1.5em';
+
+    tourVideo.style.height = '182px';
+    tourVideo.style.width = '218px';
+    tourVideo.style.border = 'solid black 1px';
+
+    instructions.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
     instructions.style.width = '200px';
+
     progressButton.setAttribute('label', 'Show me how');
     progressButton.style.display = 'block';
     progressButton.style.width = '200px';
     progressButton.style.height = '50px';
+    progressButton.style.backgroundColor = '#0996f8';
+    progressButton.style.borderColor = '#0670cc';
+    progressButton.style.color = '#fff';
+    progressButton.style.MozAppearance = 'none';
+
+    dismissLabel.textContent = 'not now';
+    dismissLabel.style.color = 'blue';
+    dismissLabel.style.textAlign = 'center';
+
+    outerbox.style.transition = 'opacity 1s ease-in-out';
+
     let xpos;
     let outerRect = {};
 
-
+    //TODO: ensure this is animating quickly enough by using timestamps too
     let movePanelToFind = () => {
       xpos -= 2;
       panel.moveTo(xpos, outerRect.y);
       //move amount of pixels center of find-input is away from center of pin-button
       if (xpos > outerRect.x - 104) {
         win.requestAnimationFrame(movePanelToFind);
+      } else {
+        outerbox.style.opacity = '1';
+        tourTitle.textContent = 'Find That Tab';
       }
     };
 
@@ -125,24 +151,31 @@ function firstInstallTour(win) {
       //move amount of pixels center of top-tabs-button is away from center of find-input
       if (xpos < outerRect.x + 69) {
         win.requestAnimationFrame(movePanelToTop);
+      } else {
+        outerbox.style.opacity = '1';
+        tourTitle.textContent = 'Easy In, Easy Out';
+        progressButton.setAttribute('label', 'Got it!');
       }
     };
 
     progressButton.onclick = (e) => {
       panel.hidePopup();
+      outerbox.removeChild(dismissLabel);
       document.getElementById('side-tabs-button').onclick(e); //will only accept left click...
       document.getElementById('mainPopupSet').appendChild(panel); //reattach to DOM after running unload
+      tourTitle.textContent = 'The Space You Need';
       progressButton.setAttribute('label', 'Next');
       panel.openPopup(document.getElementById('pin-button'), 'bottomcenter topleft', 0, 0, false, false);
       progressButton.onclick = (e) => {
+        outerbox.style.opacity = '0';
         outerRect = panel.getOuterScreenRect();
         xpos = outerRect.x;
         win.requestAnimationFrame(movePanelToFind);
         progressButton.onclick = (e) => {
+          outerbox.style.opacity = '0';
           outerRect = panel.getOuterScreenRect();
           xpos = outerRect.x;
           win.requestAnimationFrame(movePanelToTop);
-          progressButton.setAttribute('label', 'Finish');
           progressButton.onclick = (e) => {
             panel.hidePopup();
           };
@@ -150,9 +183,16 @@ function firstInstallTour(win) {
       };
     };
 
+    dismissLabel.onclick = () => {
+      panel.hidePopup();
+    };
+
     panel.appendChild(outerbox);
+    outerbox.appendChild(tourTitle);
+    outerbox.appendChild(tourVideo);
     outerbox.appendChild(instructions);
     outerbox.appendChild(progressButton);
+    outerbox.appendChild(dismissLabel);
     panel.openPopup(button, 'bottomcenter topright', 0, 0, false, true);
   }
 }
